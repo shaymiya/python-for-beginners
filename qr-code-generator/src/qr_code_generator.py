f70b474a37827fb0ef2afd7e76a86042b4d45a77
Shay <shaymiyavt@gmail.com>
2026-03-21 19:47:05 +0200

    both venv and qrcode generation work, assignment complete

--- qr-code-generator/src/qr_code_generator.py
+++ qr-code-generator/src/qr_code_generator.py
@@ -1,4 +1,19 @@
 # to use virtual environments in vscode, add the file to python projects!
 import qrcode
 
-print('qrcode imported')
+# list of common img subfixes
+image_files = ('.png', '.jpg')
+
+data = input('Enter the  text or URL: ').strip() # remove any whitespaces around the input
+filename = input('Enter the filename: ').strip()
+
+qr = qrcode.QRCode(box_size=10, border=4)
+qr.add_data(data)
+image = qr.make_image(fill_color='green', back_color='white')
+
+if filename[-4:] not in image_files:
+    filename = f'{filename}.png' # add .png to the filename
+
+image.save(filename)
+
+print(f'QR code saved as {filename}')
